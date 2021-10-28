8b6f44dd6ee325ee77f0269373fae9d811c2248e
kir931028 <kir931028@gmail.com>
2021-10-28 10:55:15 +0900

    update input선택에 따른 동작 추가 중

--- src/screens/Diary/Diary.s.ts
+++ src/screens/Diary/Diary.s.ts
@@ -15,17 +15,20 @@ export const GoodWardWrap = styled.View<{ focus: boolean }>`
   ${({ focus }) => focus && { display: 'none' }}
 `;
 
-export const WritingWrap = styled.View``;
+export const WritingWrap = styled.View<{ area: boolean }>`
+  ${({ area }) => area && { display: 'none' }}
+`;
 
 export const WritingToggleWrap = styled.TouchableOpacity`
   margin: 24px 0;
 `;
 
-export const WritingArea = styled.TextInput`
+export const WritingArea = styled.TextInput<{ area: boolean }>`
   height: 200px;
   border-radius: 8px;
   background-color: #fff;
   padding: 16px;
   font-family: ${({ theme }) => theme.regular};
   color: ${({ theme }) => theme.primaryColor};
+  ${({ area }) => area && { display: 'none' }}
 `;
--- src/screens/Diary/Diary.tsx
+++ src/screens/Diary/Diary.tsx
@@ -18,6 +18,7 @@ const Diary = (): React.ReactElement => {
   const navigation = useNavigation();
   const day = dayjs().day();
   const [focus, setFocus] = useState(false);
+  const [area, setArea] = useState('');
 
   const DayTitle = () => <Text>{`${day}日`}</Text>;
   const headerRight = () => <Text>Right</Text>;
@@ -29,8 +30,19 @@ const Diary = (): React.ReactElement => {
     });
   }, [day]);
 
-  const onFocus = () => {
-    setFocus((prev) => !prev);
+  const onInputAreaToggle = (type: string) => () => {
+    if (focus) {
+      setFocus(false);
+      return Keyboard.dismiss();
+    }
+    if (type === 'sunny') setArea((prev) => (prev !== 'sunny' ? 'sunny' : ''));
+    if (type === 'moon') setArea((prev) => (prev !== 'moon' ? 'moon' : ''));
+  };
+
+  const onInputToggle = (toggle: boolean, type: string) => () => {
+    setFocus(toggle);
+    if (type === 'sunny') setArea((prev) => (prev !== 'moon' ? 'moon' : ''));
+    if (type === 'moon') setArea((prev) => (prev !== 'sunny' ? 'sunny' : ''));
   };
 
   return (
@@ -39,17 +51,27 @@ const Diary = (): React.ReactElement => {
         <GoodWardWrap focus={focus}>
           <Text>Good Word</Text>
         </GoodWardWrap>
-        <WritingWrap>
-          <WritingToggleWrap>
+        <WritingWrap area={area === 'sunny'}>
+          <WritingToggleWrap onPress={onInputAreaToggle('sunny')}>
             <Ionicons name="sunny-outline" color="#736355" size={24} />
           </WritingToggleWrap>
-          <WritingArea multiline onFocus={onFocus} />
+          <WritingArea
+            multiline
+            onFocus={onInputToggle(true, 'sunny')}
+            onBlur={onInputToggle(false, 'sunny')}
+            area={area === 'sunny'}
+          />
         </WritingWrap>
-        <WritingWrap>
-          <WritingToggleWrap>
+        <WritingWrap area={area === 'moon'}>
+          <WritingToggleWrap onPress={onInputAreaToggle('moon')}>
             <Ionicons name="moon-outline" color="#736355" size={24} />
           </WritingToggleWrap>
-          <WritingArea multiline onFocus={onFocus} />
+          <WritingArea
+            multiline
+            onFocus={onInputToggle(true, 'moon')}
+            onBlur={onInputToggle(false, 'moon')}
+            area={area === 'moon'}
+          />
         </WritingWrap>
       </WritingWrapper>
     </SafeAreaView>
