20e93822ba0ecf4a49d8564766bd493f58d1eb41
Kir93 <cndfuf3484@naver.com>
2022-01-19 18:35:43 +0900

    update localData 변수로 수정 및 데이터 불러오기 준비

--- src/screens/Month/Month.tsx
+++ src/screens/Month/Month.tsx
@@ -1,47 +1,43 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import dayjs from 'dayjs';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import { LocaleConfig } from 'react-native-calendars';
 import { useNavigation } from '@react-navigation/native';
 import { DateData } from 'react-native-calendars/src/types';
 
+import monthLocaleData from '@utils/monthLocaleData';
+
 import AppLayout from '@components/Applayout/AppLayout';
 import { MonthCalendar } from './Month.s';
 
-LocaleConfig.locales.ko = {
-  monthNames: [
-    '1월 ',
-    '2월 ',
-    '3월 ',
-    '4월 ',
-    '5월 ',
-    '6월 ',
-    '7월 ',
-    '8월 ',
-    '9월 ',
-    '10월 ',
-    '11월 ',
-    '12월 ',
-  ],
-  monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
-  dayNames: ['일', '월', '화', '수', '목', '금', '토'],
-  dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
-  today: '오늘',
-};
+LocaleConfig.locales.ko = monthLocaleData;
 
 const Month = (): React.ReactElement => {
   LocaleConfig.defaultLocale = 'ko';
   const toDay = dayjs();
+  const toDate = toDay.toDate();
   const navigation = useNavigation();
+  const [year, setYear] = useState(dayjs().year());
+  const [month, setMonth] = useState(dayjs().month() + 1);
   const [arrow, setArrow] = useState(true);
   const sunny = { key: 'vacation', color: 'red' };
   const moon = { key: 'massage', color: 'blue' };
+
   const onDisabledArrow = useCallback(
-    (month: DateData[]) => setArrow(month[0].month === toDay.month() + 1),
+    (date: DateData[]) => setArrow(date[0].month === month),
     [arrow],
   );
   useEffect(() => {
-    navigation.setOptions({ headerTitle: `${toDay.year()} 年` });
-  }, [toDay.year()]);
+    navigation.setOptions({ headerTitle: `${year} 年` });
+  }, [year]);
+
+  useEffect(() => {
+    const getMonthData = async () => {
+      console.log(await AsyncStorage.getAllKeys());
+    };
+    getMonthData();
+  }, [toDay]);
 
   return (
     <AppLayout>
@@ -49,7 +45,7 @@ const Month = (): React.ReactElement => {
         markingType="multi-dot"
         hideExtraDays
         disableAllTouchEventsForDisabledDays
-        maxDate={toDay.toDate()}
+        maxDate={toDate}
         disableArrowRight={arrow}
         onVisibleMonthsChange={onDisabledArrow}
         markedDates={{
