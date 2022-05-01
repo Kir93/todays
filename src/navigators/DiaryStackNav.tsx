91d53ac2754aec368c640592394bd47dea4f0481
Kir93 <kir931028@gmail.com>
2022-05-01 21:49:02 +0900

    update screenOption Props ts, android title align

--- src/navigators/DiaryStackNav.tsx
+++ src/navigators/DiaryStackNav.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, StackNavigationOptions } from '@react-navigation/stack';
+
 import Diary from '@screens/Diary/Diary';
 import List from '@screens/List/List';
 import Month from '@screens/Month/Month';
 
 const Stack = createStackNavigator();
 
-const screenOptions = {
-  headerStyle: { backgroundColor: '#F2F0F0', shadowColor: '#D9CCC5' },
+const screenOptions: StackNavigationOptions = {
+  headerStyle: {
+    backgroundColor: '#F2F0F0',
+    shadowColor: '#D9CCC5',
+  },
   headerBackTitleVisible: false,
   headerLeftContainerStyle: { paddingLeft: 24 },
   headerRightContainerStyle: { paddingRight: 24 },
   headerTintColor: '#736355',
+  headerTitleAlign: 'center',
 };
 
 const DiaryStackNav = (): React.ReactElement => (
