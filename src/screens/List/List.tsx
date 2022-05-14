d1d400c9ce866e2129c83e42cee46b577188dd7e
Kir93 <kir931028@gmail.com>
2022-05-14 23:36:28 +0900

    update virtualizedList lendering success

--- src/screens/List/List.tsx
+++ src/screens/List/List.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import dayjs from 'dayjs';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
-import { TouchableOpacity, SafeAreaView, VirtualizedList } from 'react-native';
+import { View, TouchableOpacity, VirtualizedList } from 'react-native';
 
 import convertKey from '@hooks/convertKey';
 
@@ -45,31 +45,34 @@ const List = (): React.ReactElement => {
     setData(render);
   };
 
-  const onEndReached = useCallback(() => {
-    let nextMonth = month - 1;
-    let nextYear = year;
-    if (month === 1) {
-      nextMonth = 12;
-      nextYear = year - 1;
-      setYear(nextYear);
-    }
-    setMonth(nextMonth);
-    const nextDate = dayjs(`${nextYear}-${nextMonth - 1}`).daysInMonth();
-    const nextDateData = [...Array(nextDate)].map((_, i) => {
-      const id = convertKey({
-        year: nextYear.toString(),
-        month: nextMonth.toString(),
-        day: (nextDate - i).toString(),
+  const onEndReached = useCallback(
+    (nextData) => {
+      let nextMonth = month - 1;
+      let nextYear = year;
+      if (month === 1) {
+        nextMonth = 12;
+        nextYear = year - 1;
+        setYear(nextYear);
+      }
+      setMonth(nextMonth);
+      const nextDate = dayjs(`${nextYear}-${nextMonth - 1}`).daysInMonth();
+      const nextDateData = [...Array(nextDate)].map((_, i) => {
+        const id = convertKey({
+          year: nextYear.toString(),
+          month: nextMonth.toString(),
+          day: (nextDate - i).toString(),
+        });
+        return {
+          id,
+          thisDay: nextDate - i,
+          day: '',
+          moon: '',
+        };
       });
-      return {
-        id,
-        thisDay: nextDate - i,
-        day: '',
-        moon: '',
-      };
-    });
-    getMonthDates([...data, ...nextDateData]);
-  }, [data]);
+      getMonthDates([...nextData, ...nextDateData]);
+    },
+    [data],
+  );
 
   const MonthTitle = useMemo(
     () => (
@@ -105,33 +108,36 @@ const List = (): React.ReactElement => {
     getMonthDates(defaultData);
   }, []);
 
-  const keyExtractor = ({ id }: IList) => id;
+  const renderItem = ({ item, index }: { item: IList[]; index: number }) =>
+    item.length > index ? (
+      <View key={item[index].id}>
+        <DayCard onPress={onNavigateDiaryPage} {...item[index]} />
+        {item[index].id.split('-')[2] === '01' && (
+          <ListHeader>
+            {item[index].id.split('-')[1] === '01'
+              ? `${item[index].id.split('-')[0]}-${item[index].id.split('-')[1]}`
+              : item[index].id.split('-')[1]}
+            월
+          </ListHeader>
+        )}
+      </View>
+    ) : (
+      <></>
+    );
 
-  const renderItem = ({ item: { id, ...itemData } }: { item: IList }) => (
-    <React.Fragment key={id}>
-      <DayCard onPress={onNavigateDiaryPage} {...{ id, ...itemData }} />
-      {id.split('-')[2] === '01' && (
-        <ListHeader>
-          {id.split('-')[1] === '01' ? `${id.split('-')[0]}-${id.split('-')[1]}` : id.split('-')[1]}
-          월
-        </ListHeader>
-      )}
-    </React.Fragment>
-  );
+  if (!data.length) return <></>;
 
-  return data.length ? (
+  return (
     <AppLayout>
-      <SafeAreaView>
-        <VirtualizedList
-          inverted
-          onEndReachedThreshold={0.5}
-          showsVerticalScrollIndicator={false}
-          {...{ data, keyExtractor, onEndReached, renderItem }}
-        />
-      </SafeAreaView>
+      <VirtualizedList
+        inverted
+        onEndReachedThreshold={0.5}
+        showsVerticalScrollIndicator={false}
+        getItem={() => data}
+        getItemCount={() => 30}
+        {...{ data, renderItem, onEndReached }}
+      />
     </AppLayout>
-  ) : (
-    <></>
   );
 };
 
