ad4777e08f8b208a981bf49784f845930934b54c
Kir93 <kir931028@gmail.com>
2022-06-15 12:34:56 +0900

    fix import sort

--- src/screens/Month.tsx
+++ src/screens/Month.tsx
@@ -11,8 +11,9 @@ import convertKey from '@utils/convertKey';
 
 import Text from '@atoms/Text';
 import AppLayout from '@components/AppLayout/AppLayout';
-import MonthCalendar from '@components/Month/Month.styles';
+
 import A from '@components/AppLayout/AppLayout.styles';
+import MonthCalendar from '@components/Month/Month.styles';
 
 LocaleConfig.defaultLocale = 'ko';
 LocaleConfig.locales.ko = monthLocaleData;
